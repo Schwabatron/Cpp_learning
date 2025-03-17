c4ad88787f6188675cf2d2ab846305d9e0dcd40b
Jack <schwabejack04@gmail.com>
2025-03-17 16:16:47 -0400

    helloworld.cpp added, a basic intro to just printing and syntax.

---
+++ cpp_intro/helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream> //contains functions for basic input and output operations
+
+
+int main(){ //entering point for the program
+	
+	std::cout << "I like pizza" << '\n'; //newline using the newline character
+	std::cout << "It's really good!!"<< std::endl;//newline using the std::endl
+	
+
+	//newline is faster but endl will also flush the buffer
+	//personal choice
+
+
+	return 0;
+}
