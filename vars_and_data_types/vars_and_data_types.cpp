9c68070cdabce100f56ade05fc12faf3872a1d8e
Jack <schwabejack04@gmail.com>
2025-03-18 15:44:17 -0400

    removed the out file and added some more data types to the file

--- vars_and_data_types/vars_and_data_types.cpp
+++ vars_and_data_types/vars_and_data_types.cpp
@@ -17,8 +17,36 @@ int main(){
 	int year = 2025; 
 	int days = 7;
 
-	std::cout << days;
+	std::cout << days << '\n';
+	
+	//Double (number including the decimal)
 
+	double price = 3.99;
+	double gpa = 1.23;
+	double temp = 89.9234;
+
+	std::cout << price << temp << gpa << '\n';
+
+	//char(single character)
+
+	char grade = 'A';
+	char initial = 'B';
+
+	std::cout << grade << '\n';
+
+	//Boolean (true or false)
+	bool student = true;
+	bool smiling = false; 
+	
+	//String(object that represents a sequence of text)
+	
+	std::string name = "jack";
+	std::string day = "sunday";
+
+	std::cout << name << '\n';
+	std::cout << "hello " << name << " it is " << day << '\n';
+
+	
 
 	return 0;
 }
