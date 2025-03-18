a5d77b10a07216b882e87d9bb7145e6fd75a9312
Jack <schwabejack04@gmail.com>
2025-03-18 15:33:14 -0400

    testing commiting to local and pushing to remote origin from command line

--- vars_and_data_types/vars_and_data_types.cpp
+++ vars_and_data_types/vars_and_data_types.cpp
@@ -9,6 +9,16 @@ int main(){
 	x = 10;
 
 	std::cout << x << '\n'; 
+	
+	//Some data types 
+
+	//integer(whole number)
+	int age = 21; 
+	int year = 2025; 
+	int days = 7;
+
+	std::cout << days;
+
 
 	return 0;
 }
